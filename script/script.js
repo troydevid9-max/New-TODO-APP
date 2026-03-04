70b4245eadc547cee268fe3a39867cc3e2439655
troydevid9-max <troydevid9@gmail.com>
2026-03-04 16:57:09 +0100

    made masive changes

--- script/script.js
+++ script/script.js
@@ -1,124 +1,119 @@
-const taskInput = document.querySelector(".taskInput")
-const description = document.querySelector(".taskDetails")
-const submitBtn = document.querySelector(".submit")
+const taskInput = document.querySelector(".taskInput");
+const descriptionInput = document.querySelector(".taskDetails");
+const submitBtn = document.querySelector(".submit");
+const container = document.querySelector(".recntlyCreated");
+const showDiv = document.querySelector(".showDiv")
+const editMenu = document.querySelector(".editMenu")
+const newTtl = document.querySelector(".editTitle")
+const editDesc = document.querySelector("#editDesc")
+const editButton = document.querySelector(".edit")
 
-//VIEW DESC
-let holder
-let taskBox
-let descBox
 
-// const task = document.querySelector("task")
+
+
+
+
+
+let storedTask = JSON.parse(localStorage.getItem("TaskArray"))
+
+let taskArray
+if (storedTask.length > 0 && Array.isArray(storedTask)) {
+    console.log(storedTask.length)
+
+    taskArray = storedTask
+}
+else if (storedTask) {
+    taskArray = []
+    console.log(storedTask.length)
+}
+
+
+console.log(taskArray)
 let id = 0
 
-function newTask() {
 
-    const describe = document.querySelector(".taskDetails")
-    const task = document.createElement("div")
-    const dot = document.createElement("div")
-    const text = document.createElement("p")
-    const recent = document.querySelector(".recntlyCreated")
-    const info = document.querySelector(".info")
-    const icons = document.createElement("div")
-    const viewBtn = document.createElement("p")
-    const editBtn = document.createElement("p")
-    const deleteBtn = document.createElement("p")
+function addTask() {
+    if (taskInput.value.trim() === "") return;
 
-    const hold = document.createElement("div")
-    hold.classList.add("hold")
+    const task = {
 
-    holder = hold
-
-    //DESCRIPTION BOX IN TASK
-    const taskDescription = document.createElement("div")
-    const taskDescriptionH1 = document.createElement("h1")
-    taskDescriptionH1.textContent = taskInput.value
-
-    const taskDescriptionP = document.createElement("p")
-    taskDescriptionP.textContent = describe.value
-    describe.value = ""
-
-    //ADD THE CLASS NAME
-    taskDescription.classList.add("Box")
-    descBox = taskDescription
-
-    //APPEND TO THE DESCRIPTION BOX
-    taskDescription.appendChild(taskDescriptionH1)
-    taskDescription.appendChild(taskDescriptionP)
+        title: taskInput.value,
+        description: descriptionInput.value
+    };
 
 
+    taskArray.push(task);
 
+    localStorage.setItem("TaskArray", JSON.stringify(taskArray))
 
+    taskInput.value = "";
+    descriptionInput.value = "";
 
-    task.classList.add("task")
-    icons.classList.add("icons")
+    renderTask(taskArray);
+    console.log(taskArray)
+}
+submitBtn.addEventListener("click", addTask)
 
+function renderTask(tasks) {
+    const allTaskRendered = tasks.map((task, id) => {
+        return (
+            ` <div class="hold">
+                <div class="task" id="${id}">
+                    <div id="dot">
+                    </div>
+                    <div class="icons">
+                        <p onclick = "view(event)">👁️</p>
+                        <p onclick = "edit(event)">📝</p>
+                        <p onclick = "deleteTask(event)">🗑️</p>
+                    </div>
+                    <p>${task.title}</p>
+                </div>
+                <div class="Box">
+                    <h1>${task.title}</h1>
+                    <p>${task.description}</p>
+                </div>
+            </div>`
+        )
+    })
 
-    viewBtn.textContent = "👁️"
-    editBtn.textContent = "📝"
-    deleteBtn.textContent = "🗑️"
-
-
-
-    task.id = id + 1
-    dot.id = "dot"
-    text.textContent = taskInput.value
-
-    info.classList.add("notShowing")
-
-    recent.appendChild(hold)
-    task.appendChild(dot)
-    task.appendChild(icons)
-    task.appendChild(text)
-    icons.appendChild(viewBtn)
-    icons.appendChild(editBtn)
-    icons.appendChild(deleteBtn)
-    hold.appendChild(task)
-    hold.appendChild(descBox)
-
-    taskBox = task
-
-    viewBtn.addEventListener("click", view)
-    editBtn.addEventListener("click", edit)
-    deleteBtn.addEventListener("click", deleteTask)
-    taskInput.value = ""
-
-    id = id + 1
-
-    console.log("done")
+    showDiv.innerHTML = allTaskRendered
 
 }
+
+renderTask(taskArray)
+
 
 function view(e) {
-    const taskId = (e.target.parentElement.parentElement.id)
-
-    const selectedTask = document.getElementById(taskId).parentElement
-    const descriptionBox = selectedTask.querySelector(".Box")
-    console.log(descriptionBox)
-    console.log(selectedTask)
-
-    descriptionBox.classList.toggle("desShow")
-
+    const task = e.target.parentElement.parentElement.parentElement
+    task.querySelector(".Box")
+    const box = task.querySelector(".Box")
+    box.classList.toggle("desShow")
+    console.log()
 }
-
 function edit(e) {
+    const taskId = e.target.parentElement.parentElement.id
+    editMenu.classList.toggle("display")
+    const task = taskArray[taskId]
 
-    const taskId = (e.target.parentElement.parentElement.id)
-    console.log(taskId)
+    newTtl.value = task.title
+    editDesc.value = task.description
+    editButton.addEventListener("click", function done() {
+        taskArray[taskId] = {
+            title: newTtl.value,
+            description: editDesc.value
+            
+        }
+        console.log(taskId)
+        renderTask(taskArray)
+    })
 }
+
 
 function deleteTask(e) {
-    const taskId = (e.target.parentElement.parentElement.id)
-    console.log(taskId)
+    const taskId = e.target.parentElement.parentElement.id
+    taskArray.splice(taskId, 1)
+    localStorage.setItem("TaskArray", JSON.stringify(taskArray))
+    renderTask(taskArray)
+
+
 }
-
-
-
-submitBtn.addEventListener("click", newTask)
-taskInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        newTask()
-    }
-})
-
-
